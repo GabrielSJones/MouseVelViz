2e0cf1719440a60d4793373139a68195c782599a
Gabriel <toastfulboast@gmail.com>
2024-05-28 18:47:20 -0600

    fixed formatting

--- app.py
+++ app.py
@@ -23,13 +23,13 @@ def print_directions(raw_input):
         LEFT = raw_input[0][6]
         DOWN = raw_input[0][7]
         if DOWN > 0:
-            print('moving mouse upwards')
+            print("moving mouse upwards")
         elif DOWN < 0:
-            print('moving mouse downwards')
+            print("moving mouse downwards")
         if LEFT > 0:
-            print('moving mouse right')
+            print("moving mouse right")
         elif LEFT < 0:
-            print('moving mouse left')
+            print("moving mouse left")
 
 
 def track_mouse_movement():
@@ -38,27 +38,27 @@ def track_mouse_movement():
     MOUSE_MOVEMENT = get_mouse_movement_from_raw_input(events)
     time.sleep(CALCULATION_DELTA)  # DO IN NEED THIS ANYMORE?
     origin = (WIDTH / 2, HEIGHT / 2)
-    dx         = MOUSE_MOVEMENT[0]
-    dy         = MOUSE_MOVEMENT[1]
+    dx = MOUSE_MOVEMENT[0]
+    dy = MOUSE_MOVEMENT[1]
 
     if dx == 0 and dy == 0:
         return {
-            'displacement_vector': (0, 0),
-            'new_point': (WIDTH / 2, HEIGHT / 2),
-            'no_movement': True
+            "displacement_vector": (0, 0),
+            "new_point": (WIDTH / 2, HEIGHT / 2),
+            "no_movement": True,
         }
 
     else:
         return {
-            'displacement_vector': (dx, dy),
-            'new_point': ((((GRACE_FACTOR * dx)) + WIDTH / 2),
-                          (((GRACE_FACTOR * dy)) + HEIGHT / 2)),
-            'no_movement': False
+            "displacement_vector": (dx, dy),
+            "new_point": (
+                (((GRACE_FACTOR * dx)) + WIDTH / 2),
+                (((GRACE_FACTOR * dy)) + HEIGHT / 2),
+            ),
+            "no_movement": False,
         }
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     while True:
-        print(
-        track_mouse_movement()
-        )
+        print(track_mouse_movement())
